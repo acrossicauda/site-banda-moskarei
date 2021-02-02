c336348ed5567e7e231e3bf753d0fef4ea628e1e
Tiago <acrossicauda@hotmail.com.br>
2021-02-02 10:51:22 -0300

    Update font-awesome.min.css, main.css, and 20 more files...

--- moskarei/resources/views/home.php
+++ moskarei/resources/views/home.php
@@ -1,7 +1,56 @@
+<?php //require_once resource_path() . '/views/components/header.php' ?>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: tiago
- * Date: 28/12/2020
- * Time: 16:50
- */
+
+
+require_once resource_path() . '/views/components/page.php';
+
+$page = new layoutPage();
+
+
+//<!--HEADER-->
+echo $page->header();
+
+//<!--headerSession()-->
+echo $page->headerSession();
+
+//<!--homeSlider()-->
+echo $page->homeSlider();
+
+//<!--lastAlbumSession()-->
+echo $page->lastAlbumSession();
+
+//<!--biographySession()-->
+echo $page->biographySession();
+
+//<!--discographySession()-->
+echo $page->videographySession();
+
+//<!--discographySession()-->
+echo $page->discographySession();
+
+//<!--members()-->
+echo $page->members();
+
+//<!--upcomingTour()-->
+echo $page->upcomingTour();
+
+//<!--gallery()-->
+echo $page->gallery();
+
+//<!--latestBlog()-->
+echo $page->latestBlog();
+
+//<!--contact()-->
+echo $page->contact();
+
+//<!--contactAddress()-->
+echo $page->contactAddress();
+
+//<!--FOOTER-->
+echo $page->footer();
+
+//Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+?>
+
+<?php //require_once resource_path() . '/views/components/footer.php' ?>
